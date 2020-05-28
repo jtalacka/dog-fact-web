e283679ff265d7d2d278b22bd0772f840a35233e
jtalacka <julius.talacka@gmail.com>
2020-05-29 02:35:59 +0300

    Updates

---
+++ precache-manifest.404f481a38f35ca8354f915e3d883c89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f388f80a8859514660d8c8fcdf022ba",
+    "url": "/dog-fact-web/index.html"
+  },
+  {
+    "revision": "cf95d8e37e7b97e14917",
+    "url": "/dog-fact-web/static/css/main.82f3ac9f.chunk.css"
+  },
+  {
+    "revision": "ea2ab4ea2c3288bfe9b8",
+    "url": "/dog-fact-web/static/js/2.09c66894.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dog-fact-web/static/js/2.09c66894.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf95d8e37e7b97e14917",
+    "url": "/dog-fact-web/static/js/main.0214bf22.chunk.js"
+  },
+  {
+    "revision": "afb47545ebfc4d67780b",
+    "url": "/dog-fact-web/static/js/runtime-main.892cb55a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dog-fact-web/static/media/logo.5d5d9eef.svg"
+  }
+]);
